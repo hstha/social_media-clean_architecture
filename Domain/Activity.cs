c1f1a122ddef0515bcd7bf1125244ba9676e78f5
hstha <heriz.shrestha@compliancequest.com>
2021-05-06 12:28:31 +0545

    [walking-skeleton]: added activity model to domain

---
+++ Domain/Activity.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Domain
+{
+    /// <summary> Class <c>Activity</c> represents a  table of database.</summary>
+    /// <remarks> Contains properties realted to activity done in a post.</remarks>
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
