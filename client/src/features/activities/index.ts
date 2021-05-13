e5fea9111da8817705c35ce6a572ea469262ca5b
hstha <heriz.shrestha@compliancequest.com>
2021-05-13 22:52:21 +0545

    [route-store]: index for activity feature

---
+++ client/src/features/activities/index.ts
@@ -0,0 +1,5 @@
+import ActivityDashboard from './dashboard/ActivityDashboard';
+import ActivityDetail from './details/ActivityDetail';
+import ActivityForm from './form/ActivityForm';
+
+export { ActivityDashboard, ActivityDetail, ActivityForm };
