695faa1f3e7f33deacea36b08acd99e26a03d244
hstha <heriz.shrestha@compliancequest.com>
2021-05-15 12:01:34 +0545

    [error-handling]: seperated different apis into different file and added api's index.ts

--- client/src/core/api/api.ts
+++ client/src/core/api/api.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
-import { Activity } from '../interface/Activity';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+import { history } from '../..';
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -10,36 +11,52 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
-  try {
-    await sleep(1000);
-    return response;
-  } catch (err) {
-    console.error(err);
-    return await Promise.reject(err);
+  await sleep(1000);
+  return response;
+}, (error: AxiosError) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const { data, status, config } = error.response!;
+  switch (status) {
+    case 400:
+      if(typeof data === 'string') {
+        toast.error(data);
+      }else if (config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {
+        history.push('/not-found');
+      } else if (data.errors) {
+        const modalStateErrors = [];
+        for (const index in data.errors) {
+          if (data.errors[index]) {
+            modalStateErrors.push(data.errors[index]);
+          }
+        }
+        throw modalStateErrors.flat();
+      }
+      break;
+    
+    case 401:
+      toast.error('unauthorised');
+      break;
+    
+    case 404:
+      history.push('/not-found');
+      break;
+    
+    case 500:
+      toast.error('server error');
+      break;
   }
+  return Promise.reject(error);
 });
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  get: <T>(url: string): Promise<T> => axios.get<T>(url).then(responseBody),
   // eslint-disable-next-line @typescript-eslint/ban-types
-  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  post: <T>(url: string, body: {}): Promise<T> => axios.post<T>(url, body).then(responseBody),
   // eslint-disable-next-line @typescript-eslint/ban-types
-  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+  put: <T>(url: string, body: {}): Promise<T> => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string): Promise<T> => axios.delete<T>(url).then(responseBody),
 };
 
-const Activities = {
-  list: (): Promise<Activity[]> => requests.get<Activity[]>('/activities'),
-  detail: (id: string): Promise<Activity> => requests.get<Activity>(`/activities/${id}`),
-  create: (activity: Activity): Promise<void> => requests.post<void>('/activities', activity),
-  update: (activity: Activity): Promise<void> => requests.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string): Promise<void> => requests.del<void>(`/activities/${id}`)
-};
-
-const agent = {
-  Activities
-};
-
-export default agent;
+export default requests;
