6edbbb94bd1466bf7731c06000950f3cda1575c2
hstha <heriz.shrestha@compliancequest.com>
2021-05-16 18:50:59 +0545

    [identity]: added audthenticatiom, added in index and added request interceptors

---
+++ client/src/core/api/authentication.ts
@@ -0,0 +1,8 @@
+import { User, UserFormValues } from '../interface';
+import requests from './api';
+
+export const Authentication = {
+  login: ({ email, password }: UserFormValues): Promise<User> => requests.post('/account/login', {email, password}),
+  register: (user: UserFormValues): Promise<User> => requests.post('/account/register', user),
+  getCurrentUser: (): Promise<User> => requests.get('/account')
+};
