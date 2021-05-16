45cff345cb2fdbab4d0a26cc71d9c23182b23291
hstha <heriz.shrestha@compliancequest.com>
2021-05-16 18:55:36 +0545

    [identify]: added conditional rendering of buttons logic

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
+import { useStore } from '../../core/stores/store';
+import LoginForm from '../authorization/LoginForm';
+import RegisterForm from '../authorization/RegisterForm';
 
 const HomePage = (): JSX.Element => {
+  const { userStore, modalStore } = useStore();
   return (
     <Segment inverted textAlign='center' vertical className='masthead'>
       <Container text>
@@ -15,10 +19,33 @@ const HomePage = (): JSX.Element => {
           />
           Activities.
         </Header>
-        <Header as='h2' inverted content='Welcome to Activities' />
-        <Button as={Link} to='/activities' size='huge' inverted>
-          Go to Activities.
-        </Button>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as='h2' inverted content='Welcome to Activities' />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to Activities.
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              onClick={() =>
+                modalStore.openModal(<LoginForm />, 'Login to Activities')
+              }
+              size='huge'
+              inverted
+              content='Login!'
+            />
+            <Button
+              onClick={() =>
+                modalStore.openModal(<RegisterForm />, 'Register to Activities')
+              }
+              size='huge'
+              inverted
+              content='Register!'
+            />
+          </>
+        )}
       </Container>
     </Segment>
   );
