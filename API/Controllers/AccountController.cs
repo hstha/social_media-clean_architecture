1481aa4e4e230f885115ff5f328e513e7ef2c664
hstha <heriz.shrestha@compliancequest.com>
2021-05-16 18:44:05 +0545

    [identity]: added token sertvice, identity service and account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly TokenService _tokenService;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, TokenService tokenService)
+        {
+            this._tokenService = tokenService;
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+        }
+
+        [HttpPost("login", Name = "Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await this._userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await this._signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost("register", Name = "Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = await this._userManager.FindByEmailAsync(registerDto.Email);
+
+            if (user != null)
+            {
+                ModelState.AddModelError("email", "Email Already Taken");
+                return ValidationProblem();
+            }
+
+            if (user.UserName == registerDto.UserName)
+            {
+                ModelState.AddModelError("userName", "Username Already Taken");
+                return ValidationProblem();
+            }
+
+            var newUser = new User
+            {
+                Email = registerDto.Email,
+                DisplayName = registerDto.DisplayName,
+                UserName = registerDto.UserName
+            };
+
+            var result = await this._userManager.CreateAsync(newUser, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(newUser);
+            }
+            return BadRequest("Problem Registering User");
+        }
+
+        [Authorize]
+        [HttpGet(Name = "GetCurrentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await this._userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDto CreateUserObject(User user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = this._tokenService.CreateToken(user),
+                Username = user.UserName,
+                Image = null
+            };
+        }
+    }
+}
